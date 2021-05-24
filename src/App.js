ac899d3c703427c6758564b545dd0a1322466424
fanwebo <listenecho@foxmail.com>
2021-05-24 14:53:48 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -6,8 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p>React 简易模板
         </p>
         <a
           className="App-link"
